6d268cf0fc1b90c6dfa7b0b46e67ad9bd3d646f9
UpadhyeRevati04 <upadhyerevati2504@gmail.com>
2024-11-11 21:55:10 +0530

    LearnProblemSolving
    In the new CodeChef Learn module, under the "Learn Problem Solving" section, there are two courses for each language. For eg. "Python Beginner - Part 1" and "Python Beginner - Part 2". These courses help you get started with CodeChef contests.
    
    Currently there are courses for 4 languages, and hence there are 8 courses in this section. But suppose there are courses for
    N
    N languages, what will be the total number of courses in this section?

---
+++ LearnProblemSolving.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class LearnProblemSolving
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+        int input;
+        System.out.println();
+        input = sc.nextInt();
+        System.out.println(2*input);
+	}
+}
